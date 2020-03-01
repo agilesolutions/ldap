7fd4a9acd4a6f18cbcc1b067698cda0b96d09c19
Robert Rong <robert.rong@agile-solutions.ch>
2020-03-01 13:55:56 +0100

    f

--- src/main/java/com/agilesolutions/spring/config/LdapConfig.java
+++ src/main/java/com/agilesolutions/spring/config/LdapConfig.java
@@ -1,11 +1,5 @@
 package com.agilesolutions.spring.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.ldap.repository.config.EnableLdapRepositories;
-
-package com.baeldung.ldap.javaconfig;
-
-import com.baeldung.ldap.client.LdapClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -16,6 +10,8 @@ import org.springframework.core.env.Environment;
 import org.springframework.data.ldap.repository.config.EnableLdapRepositories;
 import org.springframework.ldap.core.LdapTemplate;
 import org.springframework.ldap.core.support.LdapContextSource;
+
+import com.agilesolutions.spring.LdapClient;
 
 @Configuration
 @PropertySource("classpath:application.properties")
