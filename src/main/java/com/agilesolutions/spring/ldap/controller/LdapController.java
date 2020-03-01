fae2f2f678f81f6e35823b76fb9a029541681eef
Robert Rong <robert.rong@agile-solutions.ch>
2020-03-01 13:18:35 +0100

    f

---
+++ src/main/java/com/agilesolutions/spring/ldap/controller/LdapController.java
@@ -0,0 +1,36 @@
+package com.agilesolutions.spring.ldap.controller;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * 
+ * @author u24279
+ *
+ */
+@RestController
+@RequestMapping("/api")
+public class LdapController {
+
+
+	private static final Logger logger = LoggerFactory.getLogger(LdapController.class);
+
+	/**
+	 * GET /api/version
+	 *
+	 * @return default response (status code 200)
+	 */
+	public ResponseEntity<String> showVersion() {
+
+		logger.info("Running version {} of Demo application");
+
+		return new ResponseEntity<String>("", HttpStatus.NOT_IMPLEMENTED);
+	}
+
+}
